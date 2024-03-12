4436fec493dba90bf29fc377ac20a3836fffda7e
Lixo Ten <52259786+lixoten@users.noreply.github.com>
2024-03-12 15:58:13 -0700

    Stage 4

--- HyperCinema/task/test/test.js
+++ HyperCinema/task/test/test.js
@@ -16,7 +16,7 @@ class Test extends StageTest {
     tests = [
         // Test 1 - check main-title
         this.node.execute(async () => {
-            await this.page.setViewport({width: 1440, height: 1200});
+            await this.page.setViewport({width: 1440, height: 2776});
             const title = await this.page.findBySelector('#main-title');
             return title ?
                 correct() :
@@ -89,29 +89,35 @@ class Test extends StageTest {
         }),
         //Test 9 - check section-header
         this.page.execute(() => {
-            this.actorsHeader = document.querySelector('.section-header');
+            this.actorsHeader = document.querySelectorAll('.section-header');
 
-            return this.actorsHeader ?
+            return this.actorsHeader.length === 2 ?
                 correct() :
-                wrong(`Your page should contain a .section-header element.`)
+                wrong(`Your page should contain 2 .section-header elements.`)
         }),
         //Test 10 - check font section-header
         this.page.execute(() => {
-            let actorsHeaderStyles = window.getComputedStyle(this.actorsHeader);
+            let actorsHeaderStyles = window.getComputedStyle(this.actorsHeader[0]);
+            let reviewsHeaderStyles = window.getComputedStyle(this.actorsHeader[1]);
             return actorsHeaderStyles.fontSize === '32px' && actorsHeaderStyles.fontWeight === '700' &&
-                actorsHeaderStyles.fontFamily.includes('Montserrat') ?
+                actorsHeaderStyles.fontFamily.includes('Montserrat') && reviewsHeaderStyles.fontSize === "32px" &&
+                reviewsHeaderStyles.fontWeight === '700' && reviewsHeaderStyles.fontFamily.includes('Montserrat')?
                 correct() :
-                wrong(`Check font of .section-header element.`)
+                wrong(`Check font of .section-header elements.`)
         }),
         //Test 11 - check position section-header
         this.node.execute(async () => {
             let ahCoords = await this.page.evaluate(async () => {
-                let ahObj = document.getElementsByClassName('section-header')[0];
-                return [ahObj.getBoundingClientRect().x, ahObj.getBoundingClientRect().y];
+                let actors = document.getElementsByClassName('section-header')[0];
+                let reviews = document.getElementsByClassName('section-header')[1];
+                return [actors.getBoundingClientRect().x, actors.getBoundingClientRect().y,
+                    reviews.getBoundingClientRect().x, reviews.getBoundingClientRect().y];
             });
-            return ahCoords[0] === 90 && nonStrictCompare(ahCoords[1], 786) ?
+            return ahCoords[0] === 90 && nonStrictCompare(ahCoords[1], 786) &&
+            ahCoords[2] === 90 && nonStrictCompare(ahCoords[3], 1270) ?
                 correct() :
-                wrong(`Check position of .section-header element, your positions are: x=${ahCoords[0]} and y=${ahCoords[1]}.`);
+                wrong(`Check position of .section-header element, your positions of first header are: x=${ahCoords[0]} and y=${ahCoords[1]}, and positions of second:  
+                x=${ahCoords[2]} and y=${ahCoords[3]}`);
         }),
         //Test 12 - check article
         this.page.execute(() => {
@@ -132,79 +138,141 @@ class Test extends StageTest {
                 wrong(`Check position of first article element. Now your project has the following values of first article element: 
                 x-coordinate=${arCoords[0]} and y-coordinate=${arCoords[1]}.`);
         }),
-        //Test 14 - check third article position
-        this.node.execute(async () => {
-            let arCoords = await this.page.evaluate(async () => {
-                let arObj = document.querySelectorAll('#actors-list article')[2];
-                return [arObj.getBoundingClientRect().x, arObj.getBoundingClientRect().y];
-            });
-            return nonStrictCompare(arCoords[0], 460, 5) && nonStrictCompare(arCoords[1], 865) ?
-                correct() :
-                wrong(`Check position of first article element. Now your project has the following values of third article element: 
-                x-coordinate=${arCoords[0]} and y-coordinate=${arCoords[1]}.`);
-        }),
-        //Test 15 - check article img
+        // Test 14 - check reviews-list article
         this.page.execute(() => {
-            this.imgs = document.querySelectorAll('#actors-list article img');
-
-            return this.imgs.length === 7 ?
+            this.articleObj = document.querySelectorAll('#reviews-list article');
+            this.gradeObj = document.querySelectorAll('#reviews-list article .grade');
+            return this.articleObj.length >= 3 && this.gradeObj.length >= 3 ?
                 correct() :
-                wrong(`Your page should contain 7 img elements inside articles.`)
+                wrong(`Your page must contain at least 3 reviews with 3 grades.`)
         }),
-        //Test 16 - check article img size
-        this.page.execute(() => {
-            let styles = window.getComputedStyle(this.imgs[0]);
-            return styles.width === "147px" && styles.height === "209px" ?
-                correct() :
-                wrong(`Check size of img inside article. Now your project has the following values of article img element: 
-                width=${styles.width} and height=${styles.height}.`)
-        }),
-        //Test 17 - check article size
-        this.node.execute(async () => {
-            let styles = await this.page.evaluate(async () => {
-                let arObj = document.querySelector('#actors-list article');
-                return [arObj.getBoundingClientRect().width, arObj.getBoundingClientRect().height];
-            });
-            return styles[0] === 149 && Math.round(styles[1]) === 251 ?
-                correct() :
-                wrong(`Check size of img inside article. Now your project has the following values of article element: 
-                width=${styles[0]} and height=${styles[1]}.`)
-        }),
-        //Test 18 - check article div
-        this.page.execute(() => {
-            this.divs = document.querySelectorAll('#actors-list article div');
-
-            return this.divs.length === 7 ?
-                correct() :
-                wrong(`Your page should contain 7 div elements inside articles.`)
-        }),
-        //Test 19 - check article div font
-        this.page.execute(() => {
-            let styles = window.getComputedStyle(this.divs[0]);
-            return styles.fontSize === "15px" ?
-                correct() :
-                wrong(`Check font of your article div element.`)
-        }),
-        //Test 20 - check article border
+        // Test 15 - check reviews-list article border
         this.page.execute(() => {
             let styles = window.getComputedStyle(this.articleObj[0]);
+
             return styles.border === "1px solid rgb(0, 0, 0)" && styles.borderRadius === "10px" ?
                 correct() :
-                wrong(`Check border of article.`)
+                wrong(`Please, check borders of review article.`)
         }),
-        //Test 21 - check position of 5's actors
+        // Test 16 - check position of first reviews-list article
         this.node.execute(async () => {
-            let arCoords = await this.page.evaluate(async () => {
-                let artObjs = document.querySelectorAll('#actors-list article');
-                artObjs[0].remove();
-                artObjs[1].remove();
-                let arObj = artObjs[4];
-                return [arObj.getBoundingClientRect().x, arObj.getBoundingClientRect().y];
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article');
+                return [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y];
             });
-            return nonStrictCompare(arCoords[0], 643, 5) && nonStrictCompare(arCoords[1], 865) ?
+            return coords[0] === 90 && nonStrictCompare(coords[1], 1348) ?
                 correct() :
-                wrong(`Check position of your actors after removing 2 actors. Now your project has the following values of fifth article element: 
-                x-coordinate=${arCoords[0]} and y-coordinate=${arCoords[1]}.`);
+                wrong(`Please, check position of your first review, your positions now: x=${coords[0]} and ${coords[1]}.`)
+        }),
+        // Test 17 - check position of second reviews-list article
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelectorAll('#reviews-list article')[1];
+                let obj0 = document.querySelectorAll('#reviews-list article')[0];
+                return [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y, obj0.getBoundingClientRect().y, obj0.getBoundingClientRect().height];
+            });
+            return coords[0] === 90 && Math.round(coords[1] - coords[2] - coords[3]) === 40 ?
+                correct() :
+                wrong(`Please, check position of your second review. Now your project has the following values of second review element: 
+                x-coordinate=${coords[0]} and the distance between the second and first elements of the reviews is ${coords[1] - coords[2] - coords[3]}.`)
+        }),
+        // Test 18 - check size of first reviews-list article
+        this.node.execute(async () => {
+            let width = await this.page.evaluate(async () => {
+                let obj = document.querySelectorAll('#reviews-list article')[0];
+                return obj.getBoundingClientRect().width
+            });
+            return nonStrictCompare(width, 936, 5) ?
+                correct() :
+                wrong(`Please check the width of the element with review, your width now - ${width}px.`)
+        }),
+        // Test 19 - check position of first reviews-list article span
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article .date');
+                return obj ? [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y] : [-1, -1];
+            });
+            return nonStrictCompare(coords[0], 130, 5) && nonStrictCompare(coords[1], 1392) ?
+                correct() :
+                wrong(`Please, check position of your first .date element, your position: x=${coords[0]} and y=${coords[1]}.`)
+        }),
+        // Test 20 - check position of first reviews-list article h1
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article h1');
+                return obj ? [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y] : [-1, -1];
+            });
+            return nonStrictCompare(coords[0], 130, 5) && nonStrictCompare(coords[1], 1435) ?
+                correct() :
+                wrong(`Please, check position of your first h1 element of article, your position - x=${coords[0]} and y=${coords[1]}.`)
+        }),
+        // Test 21 - check position of first reviews-list article p
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article p');
+                return obj ? [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y] : [-1, -1];
+            });
+            return nonStrictCompare(coords[0], 130, 5) && nonStrictCompare(coords[1], 1480, 15) ?
+                correct() :
+                wrong(`Please, check position of your first p element of article, your position - x=${coords[0]} and y=${coords[1]}.`)
+        }),
+        // Test 22 - check fonts of all elements of article
+        this.page.execute(() => {
+            let date = window.getComputedStyle(document.querySelector('#reviews-list article .date'));
+            let h1Obj = window.getComputedStyle(document.querySelector('#reviews-list article h1'));
+            let pObj = window.getComputedStyle(document.querySelector('#reviews-list article p'));
+
+            let dateOk = date.fontWeight === '300' && date.fontSize === '14px' && date.opacity === '0.6';
+            let h1Ok = h1Obj.fontWeight === '600' && h1Obj.fontSize === '24px';
+            let pOk = pObj.fontWeight === '400' && pObj.fontSize === '18px';
+
+            return dateOk && h1Ok && pOk ?
+                correct() :
+                wrong(`Check fonts of all elements of review article.`)
+        }),
+
+        // Test 23 - check position of first reviews-list article grade
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article .grade');
+                return obj ? [obj.getBoundingClientRect().x, obj.getBoundingClientRect().y] : [-1, -1];
+            });
+            return nonStrictCompare(coords[0], 915, 5) && nonStrictCompare(coords[1], 1348) ?
+                correct() :
+                wrong(`Please, check position of your first .grade element of article, your position - x=${coords[0]} and y=${coords[1]}.`)
+        }),
+        // Test 24 - check background and fonts of first reviews-list article grade
+        this.page.execute(() => {
+            let styles = window.getComputedStyle(this.gradeObj[0]);
+            let spanStyle = window.getComputedStyle(document.querySelector('#reviews-list article .grade span'));
+            return styles.backgroundImage && styles.fontSize === '24px' && styles.fontWeight === '500' &&
+                spanStyle.fontSize === '14px' && spanStyle.fontWeight === '300' ?
+                correct() :
+                wrong(`Check fonts and background of grade element in article.`)
+        }),
+        // Test 25 - check size of first reviews-list article grade
+        this.node.execute(async () => {
+            let coords = await this.page.evaluate(async () => {
+                let obj = document.querySelector('#reviews-list article .grade');
+                return obj ? [obj.getBoundingClientRect().width, obj.getBoundingClientRect().height] : [-1, -1];
+            });
+            return coords[0] === 72 && coords[1] === 93 ?
+                correct() :
+                wrong(`Please, check size of article grade. Now your project has the following values of first reviews-list article grade: 
+                width=${coords[0]} and height=${coords[1]}.`)
+        }),
+
+        //Test 27 - check click on reviews-button
+        this.node.execute(async () => {
+            await this.page.setViewport({width: 1440, height: 1200});
+            const reviewsButton = await this.page.findAllBySelector('button');
+            await reviewsButton[1].click();
+            let scrollTop = await this.page.evaluate(async () => {
+                return document.querySelectorAll('.section-header')[1].getBoundingClientRect().y;
+            });
+            return  Math.round(scrollTop) === 0 ?
+                correct() :
+                wrong(`Make sure you don't forget to add a scroll to the reviews section when you click on the "Reviews" button`);
         }),
     ]
 
